5fc6c08a6bfcffe94d99d56f0ea4591ee84bbe1b
BrisiaCastrejon <brissmonse@gmail.com>
2018-07-20 08:23:42 -0500

    Funciones para login de usuario

--- src/js/index.js
+++ src/js/index.js
@@ -19,7 +19,8 @@ let $login = $('#login');
 $register.on('click', function() {
   const correo = $email.val();
   const password = $password.val();
-  const registro = firebase.auth().createUserWithEmailAndPassword(correo, password)
+  const registro = firebase.auth()
+    .createUserWithEmailAndPassword(correo, password)
     .catch(function(error) {
       // Mensaje en consola si existe error de registro
       let errorCode = error.code;
@@ -33,7 +34,8 @@ $register.on('click', function() {
 $login.on('click', function() {
   const email = $email.val();
   const password = $password.val();
-  const inicio = firebase.auth().signInWithEmailAndPassword(email, password)
+  const inicio = firebase.auth()
+    .signInWithEmailAndPassword(email, password)
     .catch(function(error) {
       // Mensaje en consola si existe error de inicio de sesion
       let errorCode = error.code;
