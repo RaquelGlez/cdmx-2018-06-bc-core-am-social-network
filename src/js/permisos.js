6c561c6edc831afc67e4f46266a776feeca2888d
Victoria Acosta <vicky.acostahidalgo@gmail.com>
2018-07-31 09:54:46 -0500

    agregando bechtmark

--- src/js/permisos.js
+++ src/js/permisos.js
@@ -1,14 +1,5 @@
 const credentials = () => {
   let config = {
-      apiKey: 'AIzaSyBURBUsmSksVn1nbBWquIWIamJlXlXTaCQ',
-      authDomain: 'diy-de-firebase.firebaseapp.com',
-      databaseURL: 'https://diy-de-firebase.firebaseio.com',
-      projectId: 'diy-de-firebase',
-      storageBucket: 'diy-de-firebase.appspot.com',
-      messagingSenderId: '184356921265'
-    };
-    firebase.initializeApp(config);        
-}
     apiKey: 'AIzaSyBURBUsmSksVn1nbBWquIWIamJlXlXTaCQ',
     authDomain: 'diy-de-firebase.firebaseapp.com',
     databaseURL: 'https://diy-de-firebase.firebaseio.com',
@@ -18,4 +9,5 @@ const credentials = () => {
   };
   firebase.initializeApp(config);
 };
+
 window.onload = credentials();
