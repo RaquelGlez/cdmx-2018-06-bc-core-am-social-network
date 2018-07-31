8829c6e0cc6a820a4d9c74165f217f9fcb6d0883
BrisiaCastrejon <brissmonse@gmail.com>
2018-07-31 15:39:21 -0500

    Funcion para cerrar sesion

--- src/js/permisos.js
+++ src/js/permisos.js
@@ -1,16 +1,6 @@
 const credentials = () => {
   let config = {
 
-      apiKey: 'AIzaSyBURBUsmSksVn1nbBWquIWIamJlXlXTaCQ',
-      authDomain: 'diy-de-firebase.firebaseapp.com',
-      databaseURL: 'https://diy-de-firebase.firebaseio.com',
-      projectId: 'diy-de-firebase',
-      storageBucket: 'diy-de-firebase.appspot.com',
-      messagingSenderId: '184356921265'
-    };
-    firebase.initializeApp(config);
-}
-
     apiKey: 'AIzaSyBURBUsmSksVn1nbBWquIWIamJlXlXTaCQ',
     authDomain: 'diy-de-firebase.firebaseapp.com',
     databaseURL: 'https://diy-de-firebase.firebaseio.com',
@@ -20,6 +10,5 @@ const credentials = () => {
   };
   firebase.initializeApp(config);
 };
-
 
 window.onload = credentials();
--- src/js/view.js
+++ src/js/view.js
@@ -1,94 +1,103 @@
- // Variables del DOM vista1
- let textInput = document.getElementById('publicText'); // input de publicacion
- let publicButton = document.getElementById('publicButton'); // boton de publicacion
- let cardPublication = document.getElementById('cardPublication'); // caja de la publicacion
- let heart = document.getElementById('heart');
- let edit = document.getElementById('edit');
- let delet = document.getElementById('delet');
+// Variables del DOM vista1
+let textInput = document.getElementById('publicText'); // input de publicacion
+let publicButton = document.getElementById('publicButton'); // boton de publicacion
+let cardPublication = document.getElementById('cardPublication'); // caja de la publicacion
+let photoButton = document.getElementById('photoButton');
  
- // Variables para Usuario
- let logedUser = document.getElementById('logedUser');
- let userProfile = document.getElementById('userProfile');
- let userHome = document.getElementById('userHome');
- let userExit = document.getElementById('userExit');
- let pPhoto = document.getElementById('pPhoto');
- let pName= document.getElementById('pName');
- let pEmail = document.getElementById('pEmail');
+// Variables para Usuario
+let logedUser = document.getElementById('logedUser');
+let userProfile = document.getElementById('userProfile');
+let userHome = document.getElementById('userHome');
+let userExit = document.getElementById('userExit');
+let pPhoto = document.getElementById('pPhoto');
+let pName = document.getElementById('pName');
+let pEmail = document.getElementById('pEmail');
+let profileImage = document.getElementById('profile-image');
+
+//  //Variables para Post
+//  let edit = document.getElementById('edit');
+//  let delet = document.getElementById('delet');
  
- // Variables globales 
- let welcomeUser;
+ 
+// Variables globales 
+let welcomeUser;
 
 const getPost = () => {
   publicButton.addEventListener('click', saveData = () =>{
     const userActive = firebase.auth().currentUser;
     const textPost = textInput.value;
     if (textPost == '') {
-      alert('No ingresaste nada😥, mejor compartenos que tienes en tu refri y presiona Enviar 👍');
+      alert('No ingresaste nada😥, compartenos que tienes en tu refri y presiona Publicar 👍');
     } else {
       const newMessageKey = firebase.database().ref().child('Mensajes').push().key;
-    let update = {
-      user:userActive.uid,
-      userName:userActive.displayName,
-      post:textPost
-    }
-    firebase.database().ref(`Mensajes/${newMessageKey}`).set(update);
-    document.getElementById('publicText').value = '';      
+      let update = {
+        user: userActive.uid,
+        userName: userActive.displayName,
+        post: textPost
+      };
+      firebase.database().ref(`Mensajes/${newMessageKey}`).set(update);
+      document.getElementById('publicText').value = '';      
     }    
   });
-  };
+};
 
-  const pruebaDeNombre = (user) => {
+const welcomeUserPost = (user) => {
   const checkUser = firebase.auth().currentUser;
   welcomeUser = checkUser.displayName;
-  logedUser.innerHTML = "Hola" + " " + welcomeUser;
-} 
-// Sirve pero da undefind para el nombre del usuario// logedUser.innerHTML = "Hola" + " " + welcomeUser + " " + "comparte tu receta";
+  logedUser.innerHTML = 'Hola' + ' ' + welcomeUser;
+}; 
 
 const getProfileUser = () => {
-  firebase.auth().onAuthStateChanged( checkStatusUser = (user) => {
-    if (user){
+  firebase.auth().onAuthStateChanged(checkStatusUser = (user) => {
+    if (user) {
       let pruebaName = user.displayName;
       let pruebaEmail = user.email;
-      let pruebaPhoto = user.photoURL  || 'https://sss.ukzn.ac.za/wp-content/uploads/2017/12/profile-placeholder.png';
+      let pruebaPhoto = user.photoURL || 'https://sss.ukzn.ac.za/wp-content/uploads/2017/12/profile-placeholder.png';
       pName.textContent = pruebaName;
       pEmail.textContent = pruebaEmail;
-      pPhoto.style.background = 'url('+pruebaPhoto+')'
+      profileImage.setAttribute('src', pruebaPhoto + '?type=large');
     }
-    pruebaDeNombre();
-  })
+    welcomeUserPost();
+  });
 
   firebase.database().ref('Mensajes')
-  .on('child_added', (newMessage)=>{
-    cardPublication.innerHTML += 
+    .on('child_added', (newMessage)=>{
+      cardPublication.innerHTML += 
     `<div id="cardPublication" class="card publication">
       <div  class="card-body">
         <p>${newMessage.val().userName}</p>
         <p>${newMessage.val().post}</p>
         <div class="text-right">
-          <a id="heart" class="a-like-btn" href="#"><i class="fas fa-heart like-btn"></i></a>
-          <a id="edit" class="btn btn-secondary form-button" href="#">Editar</a>
-          <a id= "delet" class="btn btn-danger form-button" href="#">Borrar</a>
+          <a class="btn btn-secondary form-button edit" onclick="editPost">Editar</a>
+          <a class="btn btn-danger form-button delet" onclick="deletPost">Borrar</a>
         </div>
       </div> 
-    </div>`
-})
+    </div>`;
+    });
+//  //Variables para Post
+//  let edit = document.getElementsByClassName('edit');
+//  let delet = document.getElementsByClassName('delet');
+// delet.addEventListener('click', event =>{
+//   alert();
+// })
+};
 
-}
-/* userProfile.addEventListener('click', showUser = () => {
-  alert('Aquí irá el modal');
-}) */
-
- userHome.addEventListener('click', showHome = () => {
+userHome.addEventListener('click', showHome = () => {
   location.href = '../views/view1.html';
-}) 
+}); 
 
 userExit.addEventListener('click', showExit = () => {
+  firebase.auth().signOut()
+    .then(function() {
+      console.log('Saliendo...');  
+    }).catch(function(error) {
+      console.log('error');    
+    });
   location.href = '../index.html';
-}) 
+}); 
 
 
-window.onload = function () {
+window.onload = function() {
   getPost();
-  getProfileUser();
-  //pruebaDeNombre();
-}
+  getProfileUser();  
+};
